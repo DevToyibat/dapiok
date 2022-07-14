5e556ba1cfa8848daf41b6ee34afe2b2123f8698
DevToyibat <adebakintoyibat1@gmail.com>
2022-07-14 17:01:21 +0100

    initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+import Home from "./Dapiok/home";
+// import PositionTest from "./Positioning/positionTest";
+
+const App = () => {
+  return (
+    <div>
+      <Home />
+      {/* <PositionTest /> */}
+    </div>
+  );
+};
+
+export default App;
